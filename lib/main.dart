0594f5881f1b79c73d2f3405201224a44e017a91
jongdroid <qkr7627@gmail.com>
2022-06-01 23:49:38 +0900

    2022-06-01 jongdroid-commit 1. Navegator를 이용하여 화면 전환을 하였으나 코드 간결화를 위하여 Getx로 변경하였습니다. 2. 메인 페이지에서 큐알스캔을 통해 얻은 테이블 번호 데이터를 다음 페이지로 보낼 예정입니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'dart:math';
+import 'package:qrscan_for_restaurant/ordermunupage.dart';
+import 'package:get/get.dart';
 
 /** 2022-05-29 23:13PM jongdroid-개발시작
  * 매장내 테이블에 QR코드가 부착되어있음을 전제로 합니다.
@@ -9,7 +11,7 @@ import 'dart:math';
  * 기능은 점차 추가할 예정입니다. ( *실제 주문은 불가능합니다.)
  */
 
-var ranTableNum = Random().nextInt(10)+1;
+var tableNum = Random().nextInt(10) + 1;
 
 void main() {
   runApp(const MyApp());
@@ -20,9 +22,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: MainScreen(),
-
     );
   }
 }
@@ -38,21 +39,32 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('QR코드를 스캔해보세요.'),
-      centerTitle: true,),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            QrImage(
-              data: '$ranTableNum번 테이블입니다. 다음 버튼으로 메뉴를 주문해보세요.',
-              backgroundColor: Colors.white,
-              size: 200,
-            ),
-            ElevatedButton(onPressed: (){}, child: Text('다음으로')),
-          ],
+        appBar: AppBar(
+          title: Text('QR코드를 스캔해보세요.'),
+          centerTitle: true,
         ),
-      )
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              QrImage(
+                data: '$tableNum번 테이블입니다. 다음 버튼으로 메뉴를 주문해보세요.',
+                backgroundColor: Colors.white,
+                size: 200,
+              ),
+              // 큐알코드를 통해 테이블 번호 정보를 스캔하고 다음페이지에서 본격적인 주문을 할 수 있습니다.
+
+              ElevatedButton(
+                  child: Text('다음으로'),
+                  onPressed: () {
+                    //Navigator를 사용하여 화면 전환을 할 수 있지만 Get을 사용하여 더욱 간소화된 코드로 구현할 수 있습니다.
+                    Get.to(() => OrderMenuPage());
+                    Get.to(() => OrderMenuPage(), arguments: tableNum);
+                    print('$tableNum 번 테이블');
+                  },
+                ),
+            ],
+          ),
+        ));
   }
 }
