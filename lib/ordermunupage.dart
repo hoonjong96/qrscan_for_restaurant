3551107ee3a942d4d88c6d1d6ca5a494ae7d8fc2
jongdroid <qkr7627@gmail.com>
2022-06-14 00:19:05 +0900

    2022-06-14 jongdroid - 아이템 리스트를 장바구니에 담아서 볼 수 있도록 구현중이며 코드수정중에 있습니다.

--- lib/ordermunupage.dart
+++ lib/ordermunupage.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:qrscan_for_restaurant/basketPage.dart';
 
 /** 2022-06-01 23:22 jongdroid
  * 해당 페이지는 블 번메인 페이지에서 큐알 스캔을 통해 받아온 테이호로 메뉴를 주문하는 페이지 입니다. (실제 매장 주문 불가)
  * 상태관리 학습을 위한 프로젝트로 장바구니 기능을 추가할 예정입니다.
+
+ * [2022-06-14] 장바구니에 저장한 아이템 리스트를 장바구니에서 볼 수 있도록 수정중 입니다.
  */
 
 class OrderMenuPage extends StatefulWidget {
@@ -14,7 +17,7 @@ class OrderMenuPage extends StatefulWidget {
 }
 
 class _OrderMenuPageState extends State<OrderMenuPage> {
-  var tableNum = Get.arguments; //main으로 부터 얻어온 테이블 번호
+  int tableNum = Get.arguments; //main으로 부터 얻어온 테이블 번호
 
   @override
   Widget build(BuildContext context) {
@@ -26,10 +29,12 @@ class _OrderMenuPageState extends State<OrderMenuPage> {
             IconButton(
               icon: Icon(Icons.shopping_cart),
               onPressed: (){
-                print('카트가 클릭되었습니다.');
+                setState(() {
+                  // 카트 클릭시 장바구니 페이지로 전환 예정
+                  Get.to(() => BasketPage());
+                });
               },
               color: Colors.red,),
-
           ],
           centerTitle: true,
         ),
@@ -51,18 +56,19 @@ class MenuList extends StatefulWidget {
 class _MenuListState extends State<MenuList> {
 
   // 리스트 제목 = 음식 이름
-  var foodNameList = ['짜장면', '짬뽕', '볶음밥', '짬짜면',  '탕수육'];
+  List<String> foodNameList = ['짜장면', '짬뽕', '볶음밥', '짬짜면',  '탕수육' ];
   // 리스트 부제목 = prize
-  var foodPrizeList = [200, 300, 200, 300, 500];
+  List<int> foodPrizeList = [200, 300, 200, 300, 500];
   // leading = 음식 image
   var foodImageList = ['image1.jpeg', 'image2.jpeg', 'image3.jpeg', 'image4.jpeg', 'image5.jpeg' ];
 
   bool checkCartItem = false;
 
-  List<int> like = [0,0,0,0,0];
+  List<Icon> item = [Icon(Icons.remove_shopping_cart), Icon(Icons.remove_shopping_cart), Icon(Icons.remove_shopping_cart), Icon(Icons.remove_shopping_cart) , Icon(Icons.remove_shopping_cart)];
 
-  List<Icon> item = [Icon(Icons.remove_shopping_cart), Icon(Icons.remove_shopping_cart),
-    Icon(Icons.remove_shopping_cart), Icon(Icons.remove_shopping_cart) , Icon(Icons.remove_shopping_cart)];
+  List<String> saveData = ['텅 비었습니다.'];
+
+  List<String> saveData2 = [''];
 
   @override
   Widget build(BuildContext context) {
@@ -76,20 +82,19 @@ class _MenuListState extends State<MenuList> {
               child: Image.asset('image/${foodImageList[index]}')
           ),
           trailing: item[index],
-          // trailing: Icon(
-          //   checkCartItem ? Icons.shopping_cart : Icons.shopping_cart_outlined,
-          //   color: checkCartItem ? Colors.red : null,
-          // ),
           onTap: () {
-            checkCartItem = !checkCartItem;
-            if (checkCartItem == true) {
-              item[index] = Icon(Icons.shopping_cart, color: Colors.red,);
-              print('$index 번째 아이템을 클릭하였습니다.');
-            }
-            else if(checkCartItem == false) {
-              item[index] = Icon(Icons.remove_shopping_cart);
-            }
             setState(() {
+              checkCartItem =! checkCartItem;
+
+              if (checkCartItem == true) {
+                item[index] = Icon(Icons.shopping_cart, color: Colors.red,);
+                // Get.to(() => BasketPage(), arguments: foodNameList[index]);
+                // Get.to(() => BasketPage(), arguments: foodPrizeList[index]);
+                print('order : ${foodNameList[index]}');
+              }
+              else if(checkCartItem == false) {
+                item[index] = Icon(Icons.remove_shopping_cart);
+              }
             });
           },
         );
